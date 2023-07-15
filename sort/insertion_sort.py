a31907228302b40265a8ecea72d511c9a31fe195
hyunlae <gusfot@gmail.com>
2023-07-15 21:09:00 +0900

    feature: 삽입 정렬 알고리즘

---
+++ sort/insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(a):
+    n = len(a)
+    for i in range(1, n):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > key:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+
+
+d = [7, 6, 5, 2, 4, 1, 3]
+insertion_sort(d)
+print(d)
