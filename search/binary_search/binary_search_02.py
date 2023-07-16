93742b85eea9f289101c5c28b4db5bbdc2728ee0
hyunlae <gusfot@gmail.com>
2023-07-16 15:08:44 +0900

    feature: binary_search_02.py 추가

---
+++ search/binary_search/binary_search_02.py
@@ -0,0 +1,20 @@
+def binary_search(a, x):
+    n = len(a)
+    start = 0
+    end = n - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if a[mid] == x:
+            return mid
+        elif x > a[mid]:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return -1
+
+
+v = [7, 9, 17, 18, 33, 33, 42, 58, 92, 100, 178, 234, 452, 845, 1848, 1934]
+print(binary_search(v, 9))
+print(binary_search(v, 33))
+print(binary_search(v, 845))
