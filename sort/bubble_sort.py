25082d2046499525421e8531ee1b35896fb9fb35
hyunlae <gusfot@gmail.com>
2023-07-16 10:50:20 +0900

    feature: 병합 정렬 알고리

---
+++ sort/bubble_sort.py
@@ -0,0 +1,15 @@
+# 버블정렬
+
+def bubble_sort(a):
+    n = len(a)
+
+    for i in range(n - 1, 0, -1):
+        for j in range(i):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+            print(a)
+
+
+d = [7, 6, 5, 2, 4, 1, 3]
+bubble_sort(d)
+print(d)
