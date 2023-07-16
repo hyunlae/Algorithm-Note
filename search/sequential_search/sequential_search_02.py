6166105887c046bf35f405e3b7652b9de3cac05d
hyunlae <gusfot@gmail.com>
2023-07-16 11:10:46 +0900

    feature: sequential_search 패키지 추가

---
+++ search/sequential_search/sequential_search_02.py
@@ -0,0 +1,13 @@
+# 리스트에서 특정 숫자 찾기
+
+def search_list(a, x):
+    for i in range(len(a)):
+        if x == a[i]:
+            return i
+    return -1
+
+
+v = [17, 92, 18, 33, 58, 7, 33, 42]
+print(search_list(v, 18))
+print(search_list(v, 33))
+print(search_list(v, 900))
