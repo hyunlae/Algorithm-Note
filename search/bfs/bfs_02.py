68b270ca77954ec1a880e7ea8584a9de2926cab8
hyunlae <gusfot@gmail.com>
2023-07-18 20:28:37 +0900

    feature: bfs_02.py 추가

--- search/bfs/bfs_02.py
+++ search/bfs/bfs_02.py
@@ -4,25 +4,11 @@ from collections import deque
 def bfs(graph, start):
     qu = deque([start])
     visited = [start]
+
     while qu:
         v = qu.popleft()
         for i in graph[v]:
             if i not in visited:
                 qu.append(i)
                 visited.append(i)
-    print(visited)
 
-
-graph = [
-  [],
-  [2, 3, 8],
-  [1, 7],
-  [1, 4, 5],
-  [3, 5],
-  [3, 4],
-  [7],
-  [2, 6, 8],
-  [1, 7]
-]
-
-bfs(graph, 1)
